c1ba86dd65abaeb0fb8a97a3342f5152e187e56d
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2024-10-21 14:43:38 -0400

    🔧 chore: configuration of storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,9 +5,8 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack',
+    '@storybook/addon-styling-webpack'
   ],
   framework: {
     name: '@storybook/react-vite',
---
+++ .storybook/manager.ts
@@ -0,0 +1,17 @@
+import { addons } from '@storybook/manager-api';
+import { create } from '@storybook/theming';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Konstruct UI Design',
+    brandUrl: 'https://konstruct.io',
+    brandImage: '/logo.svg',
+    brandTarget: '_self',
+    colorSecondary: '#FA9247',
+    appBg: '#f0f0f0',
+    appBorderColor: '#ccc',
+    textColor: '#333',
+    barTextColor: '#666',
+  }),
+});
