87b599dc3c4298e1609d99a88a41ef78a3e88e73
futjesus <futjesus@users.noreply.github.com>
2024-11-04 22:50:44 +0000

    deploy: bb4f876fae69f443a5fbb86bc5e8af05752abdac

---
+++ components/ProgressBar/ProgressBar.d.ts
@@ -0,0 +1,3 @@
+import { FC } from '../../../node_modules/react';
+import { ProgressBarProps } from './ProgressBar.types';
+export declare const ProgressBar: FC<ProgressBarProps>;
---
+++ components/ProgressBar/ProgressBar.types.d.ts
@@ -0,0 +1,8 @@
+import { VariantProps } from 'class-variance-authority';
+import { progressBarVariants } from './ProgressBar.variants';
+export interface ProgressBarProps extends VariantProps<typeof progressBarVariants> {
+    className?: string;
+    label?: string;
+    percent: number;
+    status?: 'success' | 'progress';
+}
