c3236481b318ec49e64a19ccdadcc3c2e67012e6
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2024-11-06 15:09:38 -0500

    ✨ feat: slider component was added (#15)
    * ✨ feat: slider component was added
    
    * ✨ chore: initial scaffold to component
    
    * ⬆️  chore: some dependencies were updated
    
    * ✨ chore: first steps to implement radix ui were added
    
    * ✨ chore: more features to slider components were added

---
+++ lib/components/Slider/Slider.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Slider as SliderComponent } from './Slider';
+
+type Story = StoryObj<typeof SliderComponent>;
+
+const meta: Meta<typeof SliderComponent> = {
+  component: SliderComponent,
+};
+
+export const Slider: Story = {
+  args: {
+    showValue: true,
+  },
+  render: (args) => (
+    <div className="max-w-[350px]">
+      <SliderComponent
+        theme="kubefirst"
+        label="Slider with Kubefirst theme"
+        defaultValue={[50]}
+        {...args}
+      />
+
+      <div className="mt-10" />
+
+      <SliderComponent
+        theme="konstruct"
+        label="Slider with Konstruct theme"
+        defaultValue={[77]}
+        {...args}
+      />
+
+      <div className="mt-10" />
+
+      <SliderComponent
+        theme="colony"
+        label="Slider with Colony theme"
+        defaultValue={[35]}
+        {...args}
+      />
+    </div>
+  ),
+};
+
+export default meta;
---
+++ lib/components/Slider/Slider.tsx
@@ -0,0 +1,75 @@
+import {
+  ElementRef,
+  FC,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
+import { Root, Thumb, Track } from '@radix-ui/react-slider';
+import { twMerge } from 'tailwind-merge';
+
+import { useTheme } from '../../contexts';
+
+import { SliderProps } from './Slider.types';
+import {
+  sliderVariants,
+  thumbVariants,
+  trackVariants,
+} from './Slider.variants';
+
+export const Slider: FC<SliderProps> = forwardRef<
+  ElementRef<'input'>,
+  SliderProps
+>(
+  (
+    { label, defaultValue = [0], name, theme, size, showValue, ...delegated },
+    ref,
+  ) => {
+    const inputRef = useRef<ElementRef<'input'>>(null);
+    const [value, setValue] = useState<number[]>(defaultValue);
+    const { theme: contextTheme } = useTheme();
+    const inheritTheme = theme ?? contextTheme;
+
+    useImperativeHandle(ref, () => inputRef.current!, [inputRef]);
+
+    useEffect(() => {
+      if (inputRef.current) {
+        inputRef.current.value = value.toString();
+      }
+    }, [value]);
+
+    const handleValueChange = useCallback(
+      (newValue: number[]) => setValue(newValue),
+      [],
+    );
+
+    return (
+      <div className="w-full relative flex flex-col gap-3">
+        <div
+          className={twMerge(
+            'flex items-center',
+            label ? 'justify-between' : 'justify-end',
+          )}
+        >
+          {label ? <label>{label}</label> : null}
+          {showValue ? <span className="text-xs">{value}%</span> : null}
+        </div>
+
+        <input ref={inputRef} name={name} className="hidden" type="number" />
+
+        <Root
+          value={value}
+          className={sliderVariants({ theme: inheritTheme })}
+          onValueChange={handleValueChange}
+          {...delegated}
+        >
+          <Track className={trackVariants({ theme: inheritTheme, size })} />
+          <Thumb className={thumbVariants({ theme: inheritTheme, size })} />
+        </Root>
+      </div>
+    );
+  },
+);
