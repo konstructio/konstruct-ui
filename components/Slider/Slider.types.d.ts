9a9ad219c5b88b7f5d7c259a23602a5bcecc2195
futjesus <futjesus@users.noreply.github.com>
2024-11-06 20:10:20 +0000

    deploy: c3236481b318ec49e64a19ccdadcc3c2e67012e6

---
+++ components/Slider/Slider.types.d.ts
@@ -0,0 +1,11 @@
+import { VariantProps } from 'class-variance-authority';
+import { SliderProps as SliderPropsBase } from '@radix-ui/react-slider';
+import { sliderVariants } from './Slider.variants';
+export interface SliderProps extends VariantProps<typeof sliderVariants> {
+    className?: string;
+    defaultValue?: SliderPropsBase['defaultValue'];
+    label?: string;
+    name?: string;
+    showValue?: boolean;
+    size?: 'sm' | 'md' | 'lg';
+}
