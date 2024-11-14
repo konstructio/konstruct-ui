368fbc04b028685dfe3ff8071532d69d1ef1fb02
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2024-11-14 12:22:46 -0500

    ✨ feat: toast component was added (#22)
    * ✨ feat: toast component was added
    
    * ✨ chore: first step to implement toast component
    
    * ⬆️  chore: some dependencies have been updated
    
    * ✨ chore: more functionalities have been added to the toast component

---
+++ lib/components/Toast/Toast.tsx
@@ -0,0 +1,96 @@
+import {
+  FC,
+  isValidElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import {
+  Action,
+  Description,
+  Provider,
+  Root,
+  Title,
+  Viewport,
+} from '@radix-ui/react-toast';
+import { Slot } from '@radix-ui/react-slot';
+import { X } from 'react-feather';
+
+import { useTheme } from '../../contexts';
+
+import { ToastProps } from './Toast.types';
+import {
+  closeToastVariants,
+  toastVariants,
+  viewportToastVariants,
+} from './Toast.variants';
+
+export const Toast: FC<ToastProps> = ({
+  title,
+  duration = 5000,
+  titleClassName,
+  descriptionClassName,
+  description,
+  children,
+  theme,
+  showCloseButton = true,
+  className,
+}) => {
+  const [open, setOpen] = useState(false);
+  const timerRef = useRef(0);
+  const { theme: contextTheme } = useTheme();
+  const inheritTheme = theme ?? contextTheme;
+
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
+  const titleResult = useMemo(() => {
+    if (isValidElement(title)) {
+      return <Slot className={titleClassName}>{title}</Slot>;
+    }
+
+    return <h6 className={titleClassName}>{title}</h6>;
+  }, [title, titleClassName]);
+
+  const descriptionResult = useMemo(() => {
+    if (isValidElement(description)) {
+      return <Slot className={descriptionClassName}>{description}</Slot>;
+    }
+
+    return <h6 className={descriptionClassName}>{description}</h6>;
+  }, [description, descriptionClassName]);
+
+  const handleClick = useCallback(() => {
+    setOpen(false);
+    timerRef.current = window.setTimeout(() => setOpen(true), 100);
+  }, []);
+
+  return (
+    <Provider swipeDirection="right" duration={duration}>
+      <Slot onClick={handleClick}>{children}</Slot>
+
+      <Root
+        className={toastVariants({ theme: inheritTheme, className })}
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <Title asChild className="text-lg mb-2 font-semibold">
+          {titleResult}
+        </Title>
+
+        <Description asChild>{descriptionResult}</Description>
+
+        {showCloseButton && (
+          <Action className="absolute" asChild altText="Close the toast">
+            <X className={closeToastVariants({ theme: inheritTheme })} />
+          </Action>
+        )}
+      </Root>
+
+      <Viewport className={viewportToastVariants({ theme: inheritTheme })} />
+    </Provider>
+  );
+};
---
+++ lib/components/Toast/Toast.types.ts
@@ -0,0 +1,16 @@
+import { PropsWithChildren, ReactNode } from 'react';
+import { VariantProps } from 'class-variance-authority';
+
+import { toastVariants } from './Toast.variants';
+
+export interface ToastProps
+  extends PropsWithChildren,
+    VariantProps<typeof toastVariants> {
+  className?: string;
+  description: string | ReactNode;
+  descriptionClassName?: string;
+  duration?: number;
+  showCloseButton?: boolean;
+  title: string | ReactNode;
+  titleClassName?: string;
+}
