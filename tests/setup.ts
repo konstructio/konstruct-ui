6fb65e1f049ede6b9a243bd150d398e86bf88e1d
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2024-11-18 12:16:00 -0500

    ✅ chore: tests for checkbox were added

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,12 @@
+import { vitest } from 'vitest';
 import '@testing-library/jest-dom/vitest';
+
+// Mock the ResizeObserver
+const ResizeObserverMock = vitest.fn(() => ({
+  observe: vitest.fn(),
+  unobserve: vitest.fn(),
+  disconnect: vitest.fn(),
+}));
+
+// Stub the global ResizeObserver
+vitest.stubGlobal('ResizeObserver', ResizeObserverMock);
