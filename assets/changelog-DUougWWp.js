8600af92dd4f3b560591e21a8a62e2371903902a
futjesus <futjesus@users.noreply.github.com>
2024-12-19 02:55:36 +0000

    deploy: 9e539a332bc9b36a29e1fd59f62c3d56d4cc0a73

--- assets/changelog-DUougWWp.js
+++ assets/changelog-DUougWWp.js
@@ -1,4 +1,26 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"./index-DwOlTBOG.js";import{ao as n,ap as c}from"./index-C3KzyAT0.js";import"./index-2yJIXLcc.js";import"./iframe-Cxy08umF.js";import"../sb-preview/runtime.js";import"./index-BIKhx-Qu.js";import"./index-Bn05qqr6.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const i=`# Changelog
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"./index-DwOlTBOG.js";import{ao as n,ap as c}from"./index-DB35ovzd.js";import"./index-2yJIXLcc.js";import"./iframe-iWUjcpaZ.js";import"../sb-preview/runtime.js";import"./index-BIKhx-Qu.js";import"./index-Bn05qqr6.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const i=`# Changelog
+
+## [0.0.5](https://github.com/konstructio/konstruct-ui/tree/0.0.5) (2024-12-19)
+
+[Full Changelog](https://github.com/konstructio/konstruct-ui/compare/0.0.4...0.0.5)
+
+**Merged pull requests:**
+
+- 👷 ci: change the github action configuration [\\#38](https://github.com/konstructio/konstruct-ui/pull/38) ([futjesus](https://github.com/futjesus))
+- 👷 ci: change the github action configuration [\\#37](https://github.com/konstructio/konstruct-ui/pull/37) ([futjesus](https://github.com/futjesus))
+- 👷 ci: change the github action configuration [\\#36](https://github.com/konstructio/konstruct-ui/pull/36) ([futjesus](https://github.com/futjesus))
+- 👷 ci: change the github action configuration [\\#35](https://github.com/konstructio/konstruct-ui/pull/35) ([futjesus](https://github.com/futjesus))
+
+## [0.0.4](https://github.com/konstructio/konstruct-ui/tree/0.0.4) (2024-12-19)
+
+[Full Changelog](https://github.com/konstructio/konstruct-ui/compare/0.0.3...0.0.4)
+
+**Merged pull requests:**
+
+- 👷 ci: change the github action configuration [\\#34](https://github.com/konstructio/konstruct-ui/pull/34) ([futjesus](https://github.com/futjesus))
+- 📝 docs: the documentation was updated and some thing were added on storybook [\\#33](https://github.com/konstructio/konstruct-ui/pull/33) ([futjesus](https://github.com/futjesus))
+- 🔥 chore: some files have been deleted [\\#32](https://github.com/konstructio/konstruct-ui/pull/32) ([futjesus](https://github.com/futjesus))
+- 👷 chore: new step to CI was added to send a message when a new version have been published [\\#31](https://github.com/konstructio/konstruct-ui/pull/31) ([futjesus](https://github.com/futjesus))
 
 ## [0.0.3](https://github.com/konstructio/konstruct-ui/tree/0.0.3) (2024-12-17)
 
@@ -6,7 +28,7 @@ import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"
 
 **Merged pull requests:**
 
-- 💄 chore: new colors palette was added in the konstruct plugin [\\#30](https://github.com/konstructio/konstruct-ui/pull/30) ([futjesus](https://github.com/futjesus))
+- 💄  chore: new colors palette was added in the konstruct plugin [\\#30](https://github.com/konstructio/konstruct-ui/pull/30) ([futjesus](https://github.com/futjesus))
 - ✅ chore: tests have been added [\\#29](https://github.com/konstructio/konstruct-ui/pull/29) ([futjesus](https://github.com/futjesus))
 - ✅ chore: tests have been added [\\#28](https://github.com/konstructio/konstruct-ui/pull/28) ([futjesus](https://github.com/futjesus))
 
@@ -23,17 +45,17 @@ import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"
 - 🎨 chore: the modal has been improved [\\#27](https://github.com/konstructio/konstruct-ui/pull/27) ([futjesus](https://github.com/futjesus))
 - ✅ feat: add testing [\\#26](https://github.com/konstructio/konstruct-ui/pull/26) ([futjesus](https://github.com/futjesus))
 - ♿️ chore: add new accesibility component [\\#25](https://github.com/konstructio/konstruct-ui/pull/25) ([futjesus](https://github.com/futjesus))
-- ⬆️ chore: some dependencies have been updated [\\#24](https://github.com/konstructio/konstruct-ui/pull/24) ([futjesus](https://github.com/futjesus))
+- ⬆️  chore: some dependencies have been updated [\\#24](https://github.com/konstructio/konstruct-ui/pull/24) ([futjesus](https://github.com/futjesus))
 - 🎨 chore: change twMerge by util function [\\#23](https://github.com/konstructio/konstruct-ui/pull/23) ([futjesus](https://github.com/futjesus))
 - ✨ feat: toast component was added [\\#22](https://github.com/konstructio/konstruct-ui/pull/22) ([futjesus](https://github.com/futjesus))
 - ✨ feat: breadcrumb component was added [\\#21](https://github.com/konstructio/konstruct-ui/pull/21) ([futjesus](https://github.com/futjesus))
 - ✨ feat: switch component was added [\\#20](https://github.com/konstructio/konstruct-ui/pull/20) ([futjesus](https://github.com/futjesus))
 - ✨ feat: tooltip component was added [\\#19](https://github.com/konstructio/konstruct-ui/pull/19) ([futjesus](https://github.com/futjesus))
 - ✨ feat: progressbar component was added [\\#17](https://github.com/konstructio/konstruct-ui/pull/17) ([futjesus](https://github.com/futjesus))
-- ⬆️ chore: some dependencies were updated [\\#16](https://github.com/konstructio/konstruct-ui/pull/16) ([futjesus](https://github.com/futjesus))
+- ⬆️  chore: some dependencies were updated [\\#16](https://github.com/konstructio/konstruct-ui/pull/16) ([futjesus](https://github.com/futjesus))
 - ✨ feat: slider component was added [\\#15](https://github.com/konstructio/konstruct-ui/pull/15) ([futjesus](https://github.com/futjesus))
 - ✨ feat: text area component [\\#10](https://github.com/konstructio/konstruct-ui/pull/10) ([futjesus](https://github.com/futjesus))
-- ⬆️ chore: some dependencies were updated [\\#9](https://github.com/konstructio/konstruct-ui/pull/9) ([futjesus](https://github.com/futjesus))
+- ⬆️  chore: some dependencies were updated [\\#9](https://github.com/konstructio/konstruct-ui/pull/9) ([futjesus](https://github.com/futjesus))
 - ✨ feat: Tag and SelectTag components were added [\\#8](https://github.com/konstructio/konstruct-ui/pull/8) ([futjesus](https://github.com/futjesus))
 - ✨ feat: radio button component was added [\\#7](https://github.com/konstructio/konstruct-ui/pull/7) ([futjesus](https://github.com/futjesus))
 - ✨ feat: number input component was added [\\#6](https://github.com/konstructio/konstruct-ui/pull/6) ([futjesus](https://github.com/futjesus))
@@ -42,6 +64,8 @@ import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"
 - ✨ feat: more features were added to autocomplete component [\\#3](https://github.com/konstructio/konstruct-ui/pull/3) ([futjesus](https://github.com/futjesus))
 - ✨ feat: autocomplete component was added [\\#2](https://github.com/konstructio/konstruct-ui/pull/2) ([futjesus](https://github.com/futjesus))
 
-\\* _This Changelog was automatically generated by [github_changelog_generator](https://github.com/github-changelog-generator/github-changelog-generator)_
+
+
+\\* *This Changelog was automatically generated by [github_changelog_generator](https://github.com/github-changelog-generator/github-changelog-generator)*
 `;function o(u){return t.jsxs(t.Fragment,{children:[t.jsx(n,{title:"Documentation/Changelog"}),`
 `,t.jsx(c,{children:i})]})}function j(u={}){const{wrapper:s}={...e(),...u.components};return s?t.jsx(s,{...u,children:t.jsx(o,{...u})}):o()}export{j as default};
