3227e15ff66f1e9ddb8bfe725341986d2dcd1bb4
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-01-15 16:35:32 -0500

    ✨ feat: implement Command context and dialog components with variants

--- lib/components/Command/components/CommandItem.tsx
+++ lib/components/Command/components/CommandItem.tsx
@@ -1,20 +1,41 @@
-import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from 'react';
+import {
+  ComponentPropsWithoutRef,
+  ComponentRef,
+  forwardRef,
+  useCallback,
+  useContext,
+} from 'react';
 import { Command as CommandPrimitive } from 'cmdk';
 import { twMerge } from 'tailwind-merge';
+
+import { CommandContext } from '../contexts/Command.context';
 
 const CommandItem = forwardRef<
   ComponentRef<typeof CommandPrimitive.Item>,
   ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive.Item
-    ref={ref}
-    className={twMerge(
-      'relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
-      className,
-    )}
-    {...props}
-  />
-));
+>(({ className, onSelect, ...props }, ref) => {
+  const { setOpen } = useContext(CommandContext);
+
+  const handleSelect = useCallback(
+    (value: string) => {
+      onSelect?.(value);
+      setOpen(false);
+    },
+    [setOpen, onSelect],
+  );
+
+  return (
+    <CommandPrimitive.Item
+      ref={ref}
+      className={twMerge(
+        'relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+        className,
+      )}
+      onSelect={handleSelect}
+      {...props}
+    />
+  );
+});
 
 CommandItem.displayName = CommandPrimitive.Item.displayName;
 
---
+++ lib/components/Command/contexts/Command.context.tsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+
+import { State } from './Command.types';
+
+const initialState: State = {
+  isOpen: false,
+  setOpen: () => {
+    throw new Error('onOpen is not implemented');
+  },
+};
+
+export const CommandContext = createContext<State>(initialState);
