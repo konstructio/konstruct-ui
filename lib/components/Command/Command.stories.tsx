3e54f00d27b1771ccea7c7301d48218719dbaf28
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-01-15 17:01:00 -0500

    ✨ feat: enhance Command component with theme selection and grouping

--- lib/components/Command/Command.stories.tsx
+++ lib/components/Command/Command.stories.tsx
@@ -1,6 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Moon, Sun, Database } from 'react-feather';
 
-import { Command as CommandComponent } from './Command';
+import { ThemeProvider, useTheme } from '../../contexts';
+
+import {
+  Command as CommandComponent,
+  CommandGroup,
+  CommandItem,
+} from './Command';
+import { CommandSeparator } from 'cmdk';
 
 type Story = StoryObj<typeof CommandComponent>;
 
@@ -10,13 +18,65 @@ const meta: Meta<typeof CommandComponent> = {
 };
 
 export const Command: Story = {
-  render: () => (
-    <>
-      <div className="max-w-[350px] flex flex-col gap-2">
-        <CommandComponent />
-      </div>
-    </>
-  ),
+  render: () => {
+    const Wrapper = () => {
+      const { theme, setTheme } = useTheme();
+
+      return (
+        <>
+          <p>
+            Current Theme: <span>{theme}</span>
+          </p>
+
+          <CommandComponent>
+            <CommandGroup heading="Theme">
+              <CommandItem onSelect={() => setTheme!('kubefirst')}>
+                <Sun />
+                Kubefirst
+              </CommandItem>
+
+              <CommandItem onSelect={() => setTheme!('colony')}>
+                <Moon />
+                Colony
+              </CommandItem>
+
+              <CommandItem onSelect={() => setTheme!('colony')}>
+                <Database />
+                Civo
+              </CommandItem>
+            </CommandGroup>
+
+            <CommandSeparator />
+
+            <CommandGroup heading="Theme">
+              <CommandItem onSelect={() => setTheme!('kubefirst')}>
+                <Sun />
+                Kubefirst
+              </CommandItem>
+
+              <CommandItem onSelect={() => setTheme!('colony')}>
+                <Moon />
+                Colony
+              </CommandItem>
+
+              <CommandItem onSelect={() => setTheme!('colony')}>
+                <Database />
+                Civo
+              </CommandItem>
+            </CommandGroup>
+          </CommandComponent>
+        </>
+      );
+    };
+
+    return (
+      <ThemeProvider theme="kubefirst">
+        <div className="max-w-[350px] flex flex-col gap-2">
+          <Wrapper />
+        </div>
+      </ThemeProvider>
+    );
+  },
 };
 
 export default meta;
