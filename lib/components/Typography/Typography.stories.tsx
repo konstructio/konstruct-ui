ec17611d477c22f5c7b826c217156adf6b3ca3cd
CristhianF7 <CristhianF7@gmail.com>
2025-02-18 16:11:20 -0500

    chore: symplifying things

--- lib/components/Typography/Typography.stories.tsx
+++ lib/components/Typography/Typography.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Typography as TypographyComponent } from './Typography';
-import { variants } from './Typography.variants';
+import { TypographyProps } from './Typography.types';
 
 type Story = StoryObj<typeof TypographyComponent>;
 
@@ -10,16 +10,33 @@ const meta: Meta<typeof TypographyComponent> = {
   component: TypographyComponent,
 };
 
+const variants = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'subtitle1',
+  'subtitle2',
+  'subtitle3',
+  'labelLarge',
+  'labelMedium',
+  'labelSmall',
+  'buttonSmall',
+  'body1',
+  'body2',
+  'body3',
+  'tooltip',
+] satisfies TypographyProps['variant'][];
+
 export const Typography: Story = {
   args: {},
   render: () => (
     <>
       <div className="w-full space-y-4">
-        {Object.keys(variants).map((variant) => (
-          <TypographyComponent
-            variant={variant as keyof typeof variants}
-            theme="kubefirst"
-          >
+        {variants.map((variant) => (
+          <TypographyComponent variant={variant} theme="kubefirst">
             {`${variant} - Almost before we knew it, we had...`}
           </TypographyComponent>
         ))}
--- lib/components/Typography/Typography.variants.ts
+++ lib/components/Typography/Typography.variants.ts
@@ -1,30 +1,28 @@
 import { cva } from 'class-variance-authority';
 
-export const variants = {
-  h1: 'text-[57px] font-normal leading-[64px]',
-  h2: 'text-[45px] font-normal leading-[52px]',
-  h3: 'text-[36px] font-normal leading-[44px]',
-  h4: 'text-[32px] font-normal leading-[40px]',
-  h5: 'text-[28px] font-normal leading-[36px]',
-  h6: 'text-[24px] font-medium leading-[32px] tracking-[0.15px]',
-  subtitle1: 'text-[22px] font-normal leading-[28px]',
-  subtitle2: 'text-[16px] font-medium leading-[24px] tracking-[0.15px]',
-  subtitle3: 'text-[14px] font-medium leading-[20px] tracking-[0.1px]',
-  labelLarge: 'text-[14px] font-normal leading-[20px] tracking-[0.1px]',
-  labelMedium:
-    'text-[12px] font-medium leading-[16px] tracking-[0.5px] uppercase',
-  labelSmall:
-    'text-[11px] font-medium leading-[16px] tracking-[0.5px] uppercase',
-  buttonSmall: 'text-[14px] font-semibold leading-[20px] tracking-[0.25px]',
-  body1: 'text-[16px] font-normal leading-[24px] tracking-[0.5px]',
-  body2: 'text-[14px] font-normal leading-[20px] tracking-[0.25px]',
-  body3: 'text-[12px] font-normal leading-[16px] tracking-[0.4px]',
-  tooltip: 'text-[14px] font-normal leading-[22px]',
-};
-
 export const typographyVariants = cva([''], {
   variants: {
-    variant: variants,
+    variant: {
+      h1: 'text-[57px] font-normal leading-[64px]',
+      h2: 'text-[45px] font-normal leading-[52px]',
+      h3: 'text-[36px] font-normal leading-[44px]',
+      h4: 'text-[32px] font-normal leading-[40px]',
+      h5: 'text-[28px] font-normal leading-[36px]',
+      h6: 'text-[24px] font-medium leading-[32px] tracking-[0.15px]',
+      subtitle1: 'text-[22px] font-normal leading-[28px]',
+      subtitle2: 'text-[16px] font-medium leading-[24px] tracking-[0.15px]',
+      subtitle3: 'text-[14px] font-medium leading-[20px] tracking-[0.1px]',
+      labelLarge: 'text-[14px] font-normal leading-[20px] tracking-[0.1px]',
+      labelMedium:
+        'text-[12px] font-medium leading-[16px] tracking-[0.5px] uppercase',
+      labelSmall:
+        'text-[11px] font-medium leading-[16px] tracking-[0.5px] uppercase',
+      buttonSmall: 'text-[14px] font-semibold leading-[20px] tracking-[0.25px]',
+      body1: 'text-[16px] font-normal leading-[24px] tracking-[0.5px]',
+      body2: 'text-[14px] font-normal leading-[20px] tracking-[0.25px]',
+      body3: 'text-[12px] font-normal leading-[16px] tracking-[0.4px]',
+      tooltip: 'text-[14px] font-normal leading-[22px]',
+    },
     theme: {
       kubefirst: 'text-zinc-700',
       colony: 'text-zinc-700',
