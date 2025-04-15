538daed478d43250a868bb1aef7f0eb5cb28c406
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-04-15 15:02:41 -0400

    ✨ chore: loading componen was updated

--- lib/components/Loading/Loading.types.ts
+++ lib/components/Loading/Loading.types.ts
@@ -1,7 +1,11 @@
 import { VariantProps } from 'class-variance-authority';
 import { InputHTMLAttributes } from 'react';
 
+import { Theme } from '@/domain/theme';
+
 import { loadingVariants } from './Loading.variants';
 
 export type LoadingProps = InputHTMLAttributes<SVGSVGElement> &
-  VariantProps<typeof loadingVariants>;
+  VariantProps<typeof loadingVariants> & {
+    theme?: Theme;
+  };
--- lib/components/Loading/Loading.variants.ts
+++ lib/components/Loading/Loading.variants.ts
@@ -1,17 +1,11 @@
 import { cva } from 'class-variance-authority';
 
-export const loadingVariants = cva(
-  ['h-8', 'w-8', 'animate-spin', 'text-inherit'],
-  {
-    variants: {
-      theme: {
-        kubefirst: 'text-kubefirst-primary',
-        colony: 'text-colony-primary',
-        civo: 'text-civo-primary',
-      },
-    },
-    defaultVariants: {
-      theme: 'kubefirst',
-    },
-  },
-);
+export const loadingVariants = cva([
+  'h-8',
+  'w-8',
+  'animate-spin',
+  'text-inherit',
+  'text-kubefirst-primary',
+  'colony:text-colony-primary',
+  'civo:text-civo-primary',
+]);
