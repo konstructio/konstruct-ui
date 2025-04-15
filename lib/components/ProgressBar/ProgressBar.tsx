1b7cc337be777060cc9c3a6fdb03a2f46929540f
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-04-15 15:31:29 -0400

    ✨ chore: progress bar component was updated

--- lib/components/ProgressBar/ProgressBar.tsx
+++ lib/components/ProgressBar/ProgressBar.tsx
@@ -1,6 +1,5 @@
 import type { FC } from 'react';
 
-import { useTheme } from '@/contexts';
 import { cn } from '@/utils';
 
 import type { ProgressBarProps } from './ProgressBar.types';
@@ -14,28 +13,23 @@ export const ProgressBar: FC<ProgressBarProps> = ({
   theme,
   percent,
   status,
-}) => {
-  const { theme: contextTheme } = useTheme();
-  const inheritTheme = theme ?? contextTheme;
-
-  return (
-    <div className="w-full">
-      <div className={cn('flex', label ? 'justify-between' : 'justify-end')}>
-        {label ? <label className="font-semibold">{label}</label> : null}
-        <span className="font-semibold">{percent}%</span>
-      </div>
-
-      <div className={cn(progressBarVariants({ theme: inheritTheme }))}>
-        <div
-          className={cn(
-            progressBarProgress({ theme: inheritTheme, status }),
-            percent > 0 && percent < 99
-              ? 'transition-width duration-500'
-              : 'transition-colors duration-0',
-          )}
-          style={{ width: `${percent}%` }}
-        />
-      </div>
+}) => (
+  <div className="w-full" data-theme={theme}>
+    <div className={cn('flex', label ? 'justify-between' : 'justify-end')}>
+      {label ? <label className="font-semibold">{label}</label> : null}
+      <span className="font-semibold">{percent}%</span>
     </div>
-  );
-};
+
+    <div className={cn(progressBarVariants())}>
+      <div
+        className={cn(
+          progressBarProgress({ status }),
+          percent > 0 && percent < 99
+            ? 'transition-width duration-500'
+            : 'transition-colors duration-0',
+        )}
+        style={{ width: `${percent}%` }}
+      />
+    </div>
+  </div>
+);
