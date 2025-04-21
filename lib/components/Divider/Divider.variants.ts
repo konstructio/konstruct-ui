560d1ccaa1cd974da381bb9535b5d3d53c737668
CristhianF7 <CristhianF7@gmail.com>
2025-04-21 12:09:15 -0500

    fix: divider

--- lib/components/Divider/Divider.variants.ts
+++ lib/components/Divider/Divider.variants.ts
@@ -1,15 +1,12 @@
 import { cva } from 'class-variance-authority';
 
-export const dividerVariants = cva(
-  ['h-[1px]', 'bg-zinc-200', 'w-full', 'border-gray-200'],
-  {
-    variants: {
-      variant: {
-        default: '',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
+export const dividerVariants = cva(['h-[1px]', 'bg-zinc-200', 'w-full'], {
+  variants: {
+    variant: {
+      default: '',
     },
   },
-);
+  defaultVariants: {
+    variant: 'default',
+  },
+});
