196d6a4149b3f2f29e6a27652b421c0e3d355c65
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-04-29 12:48:43 -0400

    ✨ feat: timepicker component

---
+++ lib/components/TimePicker/TimePicker.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TimePicker as TimePickerComponent } from './TimePicker';
+
+type Story = StoryObj<typeof TimePickerComponent>;
+
+const meta: Meta<typeof TimePickerComponent> = {
+  title: 'In Review/TimePicker',
+  component: TimePickerComponent,
+};
+
+export const TimePicker: Story = {
+  render: () => (
+    <>
+      <div className="max-w-[350px] flex flex-col gap-2">
+        <TimePickerComponent />
+      </div>
+    </>
+  ),
+};
+
+export default meta;
