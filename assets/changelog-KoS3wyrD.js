014e8fa0bce04598798985aae9085d386fb38605
futjesus <futjesus@users.noreply.github.com>
2025-05-01 15:42:40 +0000

    deploy: af42afef3d787b1efe1ea79de89de4ba35266692

--- assets/changelog-KoS3wyrD.js
+++ assets/changelog-KoS3wyrD.js
@@ -1,4 +1,4 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-ikABDTSQ.js";import{e,f as i}from"./index-BTY7thFk.js";import"./index-CDlOlYQx.js";import"./iframe-BrSNWtBb.js";import"./index-BGZCjPBa.js";import"./index-CA2ozmjx.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const c=`# Changelog
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-ikABDTSQ.js";import{e,f as i}from"./index-C4dQPK0t.js";import"./index-CDlOlYQx.js";import"./iframe-B29V2N-W.js";import"./index-BGZCjPBa.js";import"./index-CA2ozmjx.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const c=`# Changelog
 
 ## [0.1.0-alpha.2](https://github.com/konstructio/konstruct-ui/tree/0.1.0-alpha.2) (2025-04-21)
 
