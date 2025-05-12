1f2d2cfae46c856be1ebdc3987bcad3ffd073887
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-05-12 16:56:31 -0500

    ✨ chore: first steps added

--- lib/components/Filter/Filter.tsx
+++ lib/components/Filter/Filter.tsx
@@ -1,3 +1,44 @@
-export const Filter = () => {
-  return <div>Filter</div>;
-};
+import { ChevronDownIcon } from 'lucide-react';
+import { FC } from 'react';
+
+import { Button } from '@/components/Button/Button';
+import { cn } from '@/utils';
+
+import { FilterProps } from './Filter.types';
+import {
+  filterButtonIconVariants,
+  filterButtonVariants,
+  filterVariants,
+} from './Filter.variants';
+
+export const Filter: FC<FilterProps> = ({ className }) => (
+  <div className={cn(filterVariants({ className }))}>
+    <div className="relative">
+      <button className={cn(filterButtonVariants())}>
+        Status <ChevronDownIcon className={cn(filterButtonIconVariants())} />
+      </button>
+    </div>
+
+    <div className="relative">
+      <button className={cn(filterButtonVariants())}>
+        Created <ChevronDownIcon className={cn(filterButtonIconVariants())} />
+      </button>
+    </div>
+
+    <div className="relative">
+      <button className={cn(filterButtonVariants())}>
+        Type <ChevronDownIcon className={cn(filterButtonIconVariants())} />
+      </button>
+    </div>
+
+    <Button
+      type="button"
+      variant="text"
+      theme="civo"
+      appearance="compact"
+      version="alternate"
+    >
+      Reset
+    </Button>
+  </div>
+);
