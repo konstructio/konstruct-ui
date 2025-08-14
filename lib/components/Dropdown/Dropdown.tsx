8e9d963bcc16e7a98088de455193add51bb749d1
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-08-14 16:09:44 -0400

    ✨ chore: add onBlur validation when the dropdown has a change (#305)

--- lib/components/Dropdown/Dropdown.tsx
+++ lib/components/Dropdown/Dropdown.tsx
@@ -8,10 +8,21 @@ import { DropdownProps } from './Dropdown.types';
 export const Dropdown: FC<DropdownProps> = forwardRef<
   ComponentRef<'input'>,
   DropdownProps
->(({ onChange, value, error, helperText, name, ...delegated }, ref) => (
-  <DropdownProvider onChange={onChange} value={value} name={name}>
+>(({ onChange, onBlur, value, error, helperText, name, ...delegated }, ref) => (
+  <DropdownProvider
+    onChange={onChange}
+    onBlur={onBlur}
+    value={value}
+    name={name}
+  >
     <div className="relative w-full">
-      <Wrapper name={name} error={error} ref={ref} {...delegated} />
+      <Wrapper
+        name={name}
+        error={error}
+        ref={ref}
+        onBlur={onBlur}
+        {...delegated}
+      />
 
       {error ? <span className="text-xs text-red-700">{error}</span> : null}
 
