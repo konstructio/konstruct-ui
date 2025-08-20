6ff142274081a2209cd8066206068c73ac5fc454
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-08-20 12:30:35 -0400

    ✨ feat: ref was added as an optional params in Typography component

--- lib/components/Typography/Typography.tsx
+++ lib/components/Typography/Typography.tsx
@@ -1,39 +1,33 @@
-import { FC } from 'react';
+import { forwardRef, ForwardRefExoticComponent } from 'react';
 
 import { cn } from '@/utils';
 
 import { HeadingTag, TypographyProps } from './Typography.types';
 import { typographyVariants } from './Typography.variants';
-import { Slot } from '@radix-ui/react-slot';
 
-const Typography: FC<TypographyProps> = ({
-  className,
-  theme,
-  children,
-  variant,
-  component,
-  asChild = false,
-  ...delegated
-}) => {
-  const Component = asChild
-    ? Slot
-    : (component ?? (variant?.includes('h') ? (variant as HeadingTag) : 'p'));
+const Typography: ForwardRefExoticComponent<Omit<TypographyProps, 'ref'>> =
+  forwardRef<HTMLHeadingElement | HTMLParagraphElement, TypographyProps>(
+    ({ className, theme, children, variant, component, ...delegated }, ref) => {
+      const Component =
+        component ?? (variant?.includes('h') ? (variant as HeadingTag) : 'p');
 
-  return (
-    <Component
-      data-theme={theme}
-      className={cn(
-        typographyVariants({
-          className,
-          variant,
-        }),
-      )}
-      {...delegated}
-    >
-      {children}
-    </Component>
+      return (
+        <Component
+          ref={ref}
+          data-theme={theme}
+          className={cn(
+            typographyVariants({
+              className,
+              variant,
+            }),
+          )}
+          {...delegated}
+        >
+          {children}
+        </Component>
+      );
+    },
   );
-};
 
 Typography.displayName = 'Typography';
 
