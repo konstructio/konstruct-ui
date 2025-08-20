02810cd320bf727a42a8655a9ddcf79b96aafac3
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-08-20 14:34:12 -0400

    🐛 fix: error with the ref in typography component

--- lib/components/Typography/Typography.tsx
+++ lib/components/Typography/Typography.tsx
@@ -1,33 +1,35 @@
-import { forwardRef, ForwardRefExoticComponent } from 'react';
+import { forwardRef, ForwardRefExoticComponent, RefAttributes } from 'react';
 
 import { cn } from '@/utils';
 
 import { HeadingTag, TypographyProps } from './Typography.types';
 import { typographyVariants } from './Typography.variants';
 
-const Typography: ForwardRefExoticComponent<Omit<TypographyProps, 'ref'>> =
-  forwardRef<HTMLHeadingElement | HTMLParagraphElement, TypographyProps>(
-    ({ className, theme, children, variant, component, ...delegated }, ref) => {
-      const Component =
-        component ?? (variant?.includes('h') ? (variant as HeadingTag) : 'p');
+const Typography: ForwardRefExoticComponent<
+  Omit<TypographyProps, 'ref'> &
+    RefAttributes<HTMLParagraphElement | HTMLHeadingElement>
+> = forwardRef<HTMLParagraphElement | HTMLHeadingElement, TypographyProps>(
+  ({ className, theme, children, variant, component, ...delegated }, ref) => {
+    const Component =
+      component ?? (variant?.includes('h') ? (variant as HeadingTag) : 'p');
 
-      return (
-        <Component
-          ref={ref}
-          data-theme={theme}
-          className={cn(
-            typographyVariants({
-              className,
-              variant,
-            }),
-          )}
-          {...delegated}
-        >
-          {children}
-        </Component>
-      );
-    },
-  );
+    return (
+      <Component
+        ref={ref}
+        data-theme={theme}
+        className={cn(
+          typographyVariants({
+            className,
+            variant,
+          }),
+        )}
+        {...delegated}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
 
 Typography.displayName = 'Typography';
 
