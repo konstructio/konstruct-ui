6e40703a886e7530f96e11037a80b697df59c798
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-10-01 11:16:24 -0300

    💄 chore: animation on tabs component was added (#345)

--- lib/components/Tabs/Tabs.variants.ts
+++ lib/components/Tabs/Tabs.variants.ts
@@ -36,22 +36,43 @@ export const listVariants = cva(['flex'], {
   },
 });
 
-export const triggerVariants = cva(['cursor-pointer', 'h-[27px]', 'w-fit'], {
-  variants: {
-    variant: {
-      default: '',
-      active: [
-        'text-zinc-700',
-        'font-[600]',
-        'border-b-2',
-        'border-aurora-500',
-        'kubefirst:border-b-2',
-        'kubefirst:border-kubefirst-primary',
-      ],
-      inactive: 'text-slate-500 font-[600]',
+export const triggerVariants = cva(
+  [
+    'cursor-pointer',
+    'h-[27px]',
+    'w-fit',
+    'block',
+    'relative',
+    'after:absolute',
+    'after:bottom-0',
+    'after:left-0',
+    'after:right-0',
+    'after:h-[2px]',
+    'after:bg-aurora-500',
+    'after:origin-bottom',
+    'after:content-[""]',
+    'after:scale-y-0',
+    'after:transition-transform',
+    'after:duration-500',
+    'after:transform-gpu',
+    'after:will-change-transform',
+    'kubefirst:after:bg-kubefirst-primary',
+  ],
+  {
+    variants: {
+      variant: {
+        default: '',
+        active: [
+          'text-zinc-700',
+          'font-semibold',
+          'after:scale-y-100',
+          'hover:after:scale-y-140',
+        ],
+        inactive: 'text-slate-500 font-[600]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-  },
-});
+);
