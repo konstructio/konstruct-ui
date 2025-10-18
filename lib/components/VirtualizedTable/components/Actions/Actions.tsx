7f06e1e0085a7ea88b149f8313d8b42f7bdf8e15
Cristhian Fernández <CristhianF7@gmail.com>
2025-10-18 00:44:21 -0500

    feat: actions class name (#364)

--- lib/components/VirtualizedTable/components/Actions/Actions.tsx
+++ lib/components/VirtualizedTable/components/Actions/Actions.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/Button/Button';
 import { RowData } from '../../VirtualizedTable.types';
 
 import { Props } from './Actions.types';
+import { cn } from '@/utils';
 
 export const Actions = <TData extends RowData>({
   actions,
@@ -29,10 +30,13 @@ export const Actions = <TData extends RowData>({
 
       <div className="absolute top-full right-0 w-[215px] hidden group-hover:block z-10">
         <div className="bg-white mt-0.5 py-2 rounded-lg shadow-lg border border-zinc-100 animate-in fade-in-0">
-          {actions.map(({ label, onClick }) => (
+          {actions.map(({ label, onClick, className }) => (
             <Button
-              key={label}
-              className="w-full text-slate-800 cursor-pointer p-0 h-9 flex gap-2 text-sm font-normal justify-start hover:bg-gray-50 hover:text-slate-800 rounded-none px-6"
+              key={label as string}
+              className={cn(
+                'w-full text-slate-800 cursor-pointer p-0 h-9 flex gap-2 text-sm font-normal justify-start hover:bg-gray-50 hover:text-slate-800 rounded-none px-6',
+                className,
+              )}
               variant="text"
               asChild
               role="presentation"
