157186a433c5276e2bc651107e58857ccda6f06a
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2025-11-20 14:46:53 -0300

    ✨ feat: phone number component was added

---
+++ lib/components/PhoneNumberInput/PhoneNumberInput.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { PhoneNumberInput as PhoneNumberInputComponent } from './PhoneNumberInput';
+
+type Story = StoryObj<typeof PhoneNumberInputComponent>;
+
+const meta: Meta<typeof PhoneNumberInputComponent> = {
+  title: 'In Review/PhoneNumberInput',
+  component: PhoneNumberInputComponent,
+};
+
+export const PhoneNumberInput: Story = {
+  render: function PhoneNumberInputStory() {
+    return (
+      <div className="w-[350px] flex flex-col gap-4">
+        <PhoneNumberInputComponent />
+      </div>
+    );
+  },
+};
+
+export default meta;
---
+++ lib/components/PhoneNumberInput/PhoneNumberInput.tsx
@@ -0,0 +1,3 @@
+export const PhoneNumberInput = () => {
+  return <div>PhoneNumberInput</div>;
+};
