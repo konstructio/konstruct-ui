1a879f215363a5af3e8c8d844cf27554863d0f6d
Cristhian Fernández <cristhianf7@gmail.com>
2025-12-04 13:54:11 -0500

    fix: svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,18 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      include: '**/*.svg',
+      svgrOptions: {
+        exportType: 'default',
+      },
+    }),
     tailwindcss(),
     libInjectCss(),
     dts({
