176cee603ad51788154cd8d86d6650d669566cec
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2026-01-07 15:48:53 -0300

    💄 style: dark mode for timepicker component was added (#439)

--- lib/components/TimePicker/TimePicker.stories.tsx
+++ lib/components/TimePicker/TimePicker.stories.tsx
@@ -11,16 +11,36 @@ const meta: Meta<typeof TimePickerComponent> = {
 
 export const TimePicker: Story = {
   render: () => (
-    <div className="max-w-[350px] flex flex-col gap-2">
-      <TimePickerComponent name="time-12" label="Time 12 format" required />
+    <>
+      <div className="max-w-[350px] flex flex-col gap-2  p-4 h-[450px]">
+        <TimePickerComponent name="time-12" label="Time 12 format" required />
 
-      <TimePickerComponent
-        name="time-24"
-        label="Time 24 format"
-        format="24"
-        required
-      />
-    </div>
+        <div className="h-5" />
+
+        <TimePickerComponent
+          name="time-24"
+          label="Time 24 format"
+          format="24"
+          required
+        />
+      </div>
+
+      <div
+        className="max-w-[350px] flex flex-col gap-2 mt-6 bg-metal-900 rounded-xl p-4 h-[450px]"
+        data-theme="dark"
+      >
+        <TimePickerComponent name="time-12" label="Time 12 format" required />
+
+        <div className="h-5" />
+
+        <TimePickerComponent
+          name="time-24"
+          label="Time 24 format"
+          format="24"
+          required
+        />
+      </div>
+    </>
   ),
 };
 
