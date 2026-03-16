10546569fa13779cbf36c8770483c37441265377
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2026-03-16 12:46:42 -0300

    🔥 chore: remove React Compiler (babel-plugin-react-compiler) (#598)
    React Compiler causes issues with Next.js server components,
    so it's being removed from the build pipeline.

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
+    react(),
     svgr({
       include: '**/*.svg',
       svgrOptions: {
